98031b0b4b1f10b83fbd0c9327996a12096dab11
Samuel Embry <samuelembry@Samuels-MacBook-Pro.local>
2021-05-07 20:04:42 -0500

    unclean scatters of each country and correlation

--- Life-Expectancy-and-GDP-Starter/life_expectancy_gdp.py
+++ Life-Expectancy-and-GDP-Starter/life_expectancy_gdp.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt 
 import seaborn as sns
+from scipy.stats import pearsonr
 
 #inputting data
 data = pd.read_csv('Life-Expectancy-and-GDP-Starter/all_data.csv')
@@ -45,6 +46,33 @@ def GDP_by_country(df, country_list):
     plt.show()
 # GDP_by_country(data, countries)
 
+#Life Expectancy v GDP scatter
 plt.scatter(data.Expectancy, data.GDP, color='orange')
 plt.title('Life Expectancy vs. Country GDP')
+plt.xlabel('Life Expectancy')
+plt.ylabel('GDP')
 plt.show()
+
+#correlation of Life Expectancy v GDP
+r, pval = pearsonr(data.Expectancy, data.GDP)
+print(r, pval)
+
+#all countries Life expectancy v GDP individually plotted
+plt.clf()
+fig, axs = plt.subplots(3, 2)
+axs[0,0].scatter(chile.Expectancy, chile.GDP, color = 'red')
+axs[0,0].set_title('Chile')
+axs[0,1].scatter(china.Expectancy, china.GDP, color='blue')
+axs[0,1].set_title('China')
+axs[1,0].scatter(germany.Expectancy, germany.GDP, color='black')
+axs[1,0].set_title('Germany')
+axs[1,1].scatter(mexico.Expectancy, mexico.GDP, color='green')
+axs[1,1].set_title('Mexico')
+axs[2,0].scatter(usa.Expectancy, usa.GDP, color='orange')
+axs[2,0].set_title('USA')
+axs[2,1].scatter(zimbabwe.Expectancy, zimbabwe.GDP, color='pink')
+axs[2,1].set_title('Zimbabwe')
+for ax in axs.flat:
+    ax.set(xlabel='Life Expectancy', ylabel='GDP')
+plt.show()
+
